305456831ffd8289ae9611962c97358018f08b43
Arlo O'Keeffe <tiger@k5d.de>
2022-10-01 22:30:12 +0200

    add the first project setup

---
+++ src/main/java/io/github/arlol/scifimovies/SciFiMoviesApplication.java
@@ -0,0 +1,13 @@
+package io.github.arlol.scifimovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SciFiMoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SciFiMoviesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/arlol/scifimovies/SciFiMoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.arlol.scifimovies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SciFiMoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
