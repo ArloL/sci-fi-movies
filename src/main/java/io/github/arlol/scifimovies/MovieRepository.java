7a148a2e08f61b44b6de0b0af182f0841a9bb170
Arlo O'Keeffe <tiger@k5d.de>
2022-12-15 17:58:29 +0100

    remove unnecessary @Repository

--- src/main/java/io/github/arlol/scifimovies/MovieRepository.java
+++ src/main/java/io/github/arlol/scifimovies/MovieRepository.java
@@ -3,9 +3,7 @@ package io.github.arlol.scifimovies;
 import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface MovieRepository extends CrudRepository<Movie, Long> {
 
 	Optional<Movie> findByTitleAndYear(String title, int year);
